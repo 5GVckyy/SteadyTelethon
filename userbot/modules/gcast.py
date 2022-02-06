f98fe2dcdca98f41f0c7cb9a0221421ec99b9bec
5GVckyy <vckyex@gmail.com>
2022-02-07 03:49:10 +0700

    [STEADY-USB]

---
+++ userbot/modules/gcast.py
@@ -0,0 +1,80 @@
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+GCAST_BLACKLIST = [
+    -1001473548283,  # SharingUserbot
+    -1001433238829,  # TedeSupport
+    -1001476936696,  # AnosSupport
+    -1001327032795,  # UltroidSupport
+    -1001294181499,  # UserBotIndo
+    -1001419516987,  # VeezSupportGroup
+    -1001459812644,  # GeezSupportGroup
+    -1001296934585,  # X-PROJECT BOT
+    -1001481357570,  # UsergeOnTopic
+    -1001459701099,  # CatUserbotSupport
+    -1001109837870,  # TelegramBotIndonesia
+    -1001752592753,  # Skyzusupport
+    -1001456135097,  # SpamBot
+    -1001462425381,  # JANGAN DI APUS
+    -1001649802697,  # JANGAN DI APUS
+]
+
+
+@register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
+async def gcast(event):
+    xx = event.pattern_match.group(1)
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit("**Berikan Sebuah Pesan atau Reply**")
+        return
+    kk = await event.edit("`Globally Broadcasting Msg...`")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+                elif chat not in GCAST_BLACKLIST:
+                    pass
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if not xx:
+        return await event.edit("`Berikan beberapa teks untuk Siaran Global`")
+    tt = event.text
+    msg = tt[7:]
+    kk = await event.edit("`Sedang Mengirim pesan secara global...`")
+    er = 0
+    done = 0
+    async for x in bot.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(f"Berhasil Mengirim Pesan Ke `{done}` obrolan, kesalahan dalam `{er}` obrolan(s)")
+
+
+CMD_HELP.update(
+    {
+        "broadcast": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gcast`\
+         \n↳ : Mengirim Pesan Group Secara Global.\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gucast`\
+         \n↳ : Mengirim Pesan Pribadi Secara Global"
+    }
+)
