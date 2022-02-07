256e9d428a9cb6cf38d1ec26737634d64cca38c2
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-02-08 02:17:22 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -36,7 +36,7 @@ pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
 git clone https://github.com/5GVckyy/SteadyUserbot
-cd Geez-UserBot
+cd Steady-UserBot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
