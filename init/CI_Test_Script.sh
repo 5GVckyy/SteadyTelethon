f76cc47aa6c6d9d7c9d644003fc73eb40f20ae71
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-02-08 02:57:28 +0700

    Update CI_Test_Script.sh

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -21,5 +21,5 @@ git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com
 git fetch
 git checkout staging
 git pull origin staging
-git push --force origin staging: Geez-UserBot
+git push --force origin staging: Steady-Userbot
 tg_sendinfo "<code>I have merged all commits from staging into Geez-UserBot</code>"
