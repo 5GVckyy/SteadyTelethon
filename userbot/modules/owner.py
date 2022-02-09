bb2ef455d3daf97430ec7552d553c86ac03c1e9c
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-02-09 15:36:56 +0700

    Update owner.py

--- userbot/modules/owner.py
+++ userbot/modules/owner.py
@@ -7,7 +7,6 @@ from telethon.errors import rpcbaseerrors
 
 import userbot.modules.sql_helper.gban_sql as gban_sql
 from userbot import BOTLOG_CHATID
-from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, DEVS
 from userbot.events import register
 from userbot.utils import edit_or_reply
